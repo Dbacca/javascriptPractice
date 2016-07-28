7817d62e29e098ae9c894e0c5f3198f3a74aa2dc
Dbacca <cplsince@gmail.com>
2016-07-28 22:51:17 +0700

    commented
    1st round of commenting

--- remainingChars.js
+++ remainingChars.js
@@ -1,7 +1,11 @@
+//simple function using jQuery that dynamically displays  
+//the amount of characters remaining
+
 $(function () {
     var maxLength = 15;
+    //this character max is defined as an attribute in the HTML
     $('textarea').keyup(function () {
-        var textlen = maxLength - $(this).val().length;
-        $('#remainingChars').text(textlen);
+        var textlength = maxLength - $(this).val().length;
+        $('#remainingChars').text(textlength);
     });
 });
