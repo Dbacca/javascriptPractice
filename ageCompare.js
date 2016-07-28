4f248ea6ca066e1ecfcbbdf144d99cd426cb2781
Dbacca <cplsince@gmail.com>
2016-07-28 23:10:17 +0700

    commented
    1st round of commenting

--- ageCompare.js
+++ ageCompare.js
@@ -1,4 +1,4 @@
-// Our person constructor
+// person constructor creats a new object with 
 function Person(name, age) {
     this.name = name;
     this.age = age;
@@ -8,7 +8,7 @@ function Person(name, age) {
 var ageDifference = function (person1, person2) {
     return person1.age - person2.age;
 };
-// Make a new function, olderAge, to return the age of
+//function olderAge returns the age of
 // the older of two people
 var olderAge = function (person1, person2) {
     if (person1.age >= person2.age || person1.age == person2.age) {
@@ -18,7 +18,8 @@ var olderAge = function (person1, person2) {
         return person2.age;
     }
 };
-// Let's bring back alice and billy to test our new function
+
+// alice and billy are used to test the new function
 var alice = new Person("Alice", 30);
 var billy = new Person("Billy", 25);
 console.log("The older person is " + olderAge(alice, billy));
